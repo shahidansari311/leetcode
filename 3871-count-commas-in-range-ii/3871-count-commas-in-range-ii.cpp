3f0e775f1aa54ff87383488b75bd76ea4b385b9d
Shahid Ansari <shahidansari945256@gmail.com>
2026-09-10 03:49:31 +0530

    Time: 0 ms (100%), Space: 9.2 MB (18.47%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans=0;
+        long long p = 1000;
+
+        while (p <= n) {
+            ans+=n-p+1;
+            
+            if (p>n/1000)
+                break;
+            
+            p*=1000;
+        }
+        return ans;
+    }
+};
